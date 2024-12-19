d86814d7b31069b25c84ca4cba1ab95c9fca19c3
aniakici <ickiest.evident0d@icloud.com>
2024-12-18 23:21:53 -0500

    Update sta215-Kicinska.R

--- sta215-Kicinska.R
+++ sta215-Kicinska.R
@@ -1,26 +1,32 @@
-setwd("H:/sta215")
-#Load the data
-raw_data<-read.csv("raw_data.csv")
-data <- na.omit(raw_data)
+## Project:  STA 215, Fall 2024, Final Project
+# Located:   iCloud
+# File Name: Stats-Final-Project
+# Date:      2024_12_17
+# Who:       Anna Kicinska
+
+#Load packages
 #Install "haven" packag
 #install.packages("haven")
-
-#Load packageS
 library("readr")
 library("dplyr")
 library("haven")
 library("psych")
 library("ggplot2")
 
-#descriptive statistics
+#Load the data
+setwd("H:/sta215")
+raw_data<-read.csv("raw_data.csv")
+data <- na.omit(raw_data)
+
+#########################################################################################
+############### Table 1: descriptive statistics  ########################################
+#########################################################################################
 table(data$budget_cuts,data$tax)
 
 table(data$salary)
 
-
 summary(data$income)
 sd(data$income)
-
 
 summary(data$graduation)
 sd(data$graduation)
@@ -28,8 +34,9 @@ sd(data$graduation)
 summary(data$percent_of._low_income_students)
 sd(data$percent_of._low_income_students)
 
-
-
+##########################################################################################
+################ Table 2: contingency table ##############################################
+##########################################################################################
 # table 2
 table(data$tax,data$budget_cuts)
 #Chi-Squared Test(Tax & Budget Cuts)
@@ -37,12 +44,17 @@ chisq.test(data$tax,data$budget_cuts)
 result<-chisq.test(data$tax,data$budget_cuts)
 print(result)
 
+##########################################################################################
+################# Figure 1: boxplot ######################################################
+##########################################################################################
 #ANOVA
 boxplot(percent_of._low_income_students ~ salary, data = raw_data)
 anova <- aov(percent_of._low_income_students ~ salary, data = raw_data)
 summary(anova)
 
-#linear regression
+##########################################################################################
+################# Figure 2: scatter plot #################################################
+##########################################################################################
 #Scatter Plot
 plot(data$percent_of._low_income_students, data$graduation)
 #Calculate mean lines
@@ -52,14 +64,16 @@ meany <- mean(data$graduation)
 abline(v=meanx)
 abline(h=meany)
 
-       
+
 #Linear Regression
+#Add the linear regression line to the scatter plot
 model<- lm(graduation ~ percent_of._low_income_students,data=data)
 summary(model)
 abline(model)
 
-
-#residuals
-
+#########################################################################################
+############### Figure 3: residual plot #################################################
+#########################################################################################
+#Plot the residuals
 plot(data$percent_of._low_income_students,residuals(model))
 abline(h=0)
